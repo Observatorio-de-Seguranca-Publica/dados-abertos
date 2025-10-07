7de76e49083b1c2f95befa683b5a4aad7bba4749
henriquesiqr <henrique.ribeiro4488@gmail.com>
2025-10-07 11:36:28 -0300

    add arquivo veiculos recuperados

---
+++ codigos/produtividade/veiculos_recuperados.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+from impala.dbapi import connect
+
+# Função para ler o arquivo de credenciais
+def get_credentials(file_path):
+    credentials = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            key, value = line.strip().split('=')
+            credentials[key] = value
+    return credentials
+
+# Função para conectar ao banco de dados
+def get_conn_and_cursor(db='db_bisp_reds_reporting', credentials_file='C:/Users/x15501492/Downloads/Credenciamento Python.txt'):
+    credentials = get_credentials(credentials_file)
+    conn = connect(host='10.100.62.20', port=21051, use_ssl=True, auth_mechanism="PLAIN",
+                   user=credentials['username'], password=credentials['password'], database=db)
+    cursor = conn.cursor()
+    return conn, cursor
+
+# Função para executar query e retornar dataframe
+def executa_query_retorna_df(query, db='db_bisp_reds_reporting'):
+    conn, cursor = get_conn_and_cursor(db)  
+    cursor.execute(query)
+    results = cursor.fetchall()
+    columns = [c[0] for c in cursor.description]
+    df = pd.DataFrame(results, columns=columns)
+    conn.close()
+    return df
+
+# Função para listar tabelas no banco de dados
+def tabelas(filtro='', db='db_bisp_reds_reporting'):
+    conn, cursor = get_conn_and_cursor(db)
+    cursor.execute('SHOW TABLES')
+    tabelas_nomes = cursor.fetchall()    
+    conn.close()
+    tabelas_filtradas = [tupla_tabela[0] for tupla_tabela in tabelas_nomes if filtro in tupla_tabela[0]]
+    return tabelas_filtradas
+
+# Função para listar bancos de dados
+def bancos_de_dados():
+    conn, cursor = get_conn_and_cursor()
+    try:
+        cursor.execute("SHOW DATABASES")
+        databases = cursor.fetchall()
+        accessible_databases = []
+        for db in databases:
+            try:
+                cursor.execute(f"USE {db[0]}")
+                accessible_databases.append(db[0])
+            except:
+                pass
+        return accessible_databases
+    finally:
+        cursor.close()
+        conn.close()
+
+# Consulta ao banco (script do dbeaver: no exemplo abaixo há um join entre a tabela de ocorrências e envolvidos)
+try:
+    query = '''SELECT oco.numero_ocorrencia,
+                      vei.situacao_placa_descricao,
+                      YEAR (oco.data_hora_fato) as ano_fato,
+                      MONTH (oco.data_hora_fato) as mes_numerico_fato,
+                      oco.nome_municipio,
+                      oco.codigo_municipio
+               FROM db_bisp_reds_reporting.tb_ocorrencia AS oco
+               LEFT JOIN db_bisp_reds_reporting.tb_veiculo_ocorrencia AS vei
+                    ON oco.numero_ocorrencia = vei.numero_ocorrencia
+               WHERE oco.data_hora_fato >= '2024-01-01 00:00:00.000'
+               AND oco.data_hora_fato < '2025-09-01 00:00:00.000'
+               AND oco.ocorrencia_uf = 'MG'
+               AND vei.situacao_placa_descricao = 'RECUPERADO'
+                '''
+        
+    df = executa_query_retorna_df(query, db='db_bisp_reds_reporting')
+
+except Exception as e:
+    print(f"Erro ao consultar a tabela 'tb_ocorrencia': {e}")
+
+# Exibe as primeiras linhas do DataFrame
+df.head()
+
+# Exporta a base no computador no modelo desejado 
+df.to_excel("C:/Users/x15501492/Downloads/da_veiculos_recuperados.xlsx",index=False)
+
+print('FINALIZOU :)')
