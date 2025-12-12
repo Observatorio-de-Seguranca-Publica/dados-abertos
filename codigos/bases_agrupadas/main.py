0fec47362aecf00939f3d29f828fb04d47dc50ec
henriquesiqr <henrique.ribeiro4488@gmail.com>
2025-12-12 11:31:25 -0300

    cria arq main bases agg

---
+++ codigos/bases_agrupadas/main.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+# Caminho da pasta onde estão os scripts
+#PASTA = "C:/Users/x15501492/Documents/02 - Publicações/Códigos/Agrupados"
+PASTA = "C:/Users/x15501492/Documents/Sejusp/DIS/Dados abertos/Automatização/Agrupados"
+este_arquivo = os.path.basename(__file__)  # nome do script atual
+
+
+for arquivo in os.listdir(PASTA):
+    if arquivo.endswith(".py") and arquivo != este_arquivo:
+        caminho_script = os.path.join(PASTA, arquivo)
+        print(f"\n--- Executando: {arquivo} ---")
+        subprocess.run(["python", caminho_script], check=True)
+
+print("\n✅ Finalizado com sucesso!")
