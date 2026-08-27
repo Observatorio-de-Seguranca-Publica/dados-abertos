f8abf970cc3db01bbcfe2ab8b39509a770d98a9f
henriquesiqr <henrique.ribeiro4488@gmail.com>
2026-08-27 18:32:59 -0300

    melhora bat

--- execucao.bat
+++ execucao.bat
@@ -2,10 +2,10 @@
 
 chcp 65001 > nul
 
-cd /d "C:\Users\x15501492\Downloads\code\observatorio\dados-abertos"
+cd /d "%~dp0"
 
 if not exist logs mkdir logs
 
 set DATA=%date:~6,4%_%date:~3,2%_%date:~0,2%
 
-"C:\Program Files\Python313\python.exe" main.py >> logs\execucao_%DATA%.log 2>&1
+python main.py >> logs\execucao_%DATA%.log 2>&1
